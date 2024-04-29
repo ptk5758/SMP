6adc685733d01787fc018318d8855b64fd86551f
whisper74541 <zaq4869@naver.com>
2024-04-30 00:32:01 +0900

    잘못된 입력, 종료 추가

--- index.js
+++ index.js
@@ -23,8 +23,11 @@ rl.question("관리자님의 아이디를 입력하세요 : ", (line) => {
 const database = ["카메라", "조명", "고프로"];
 function management() {
     console.log("어서오세요 관리자님");
+    equipmentChoice();
+}
+function equipmentChoice() {
     console.log("원하시는 서비스를 입력하세요");
-    console.log("1. 장비 조회\n2. 장비 등록\n3. 장비 삭제");
+    console.log("1. 장비 조회\n2. 장비 등록\n3. 장비 삭제\n4. 종료");
     rl.question("", (line) => {
         let choice = parseInt(line);
         switch (choice) {
@@ -37,11 +40,17 @@ function management() {
             case 3: // 장비 삭제
                 console.log("장비 삭제");
                 break;
+            case 4: // 종료
+                console.log("종료 됩니다");
+                process.exit();
             default: // 잘못된 입력
+                console.log("잘못된 입력입니다.");
                 break;
         }
+        equipmentChoice();
     });
 }
+// 모든 장비를 조회합니다.
 function printAllEquipemt() {
     console.log(database);
 }
--- index.ts
+++ index.ts
@@ -22,10 +22,15 @@ const database = ["카메라", "조명", "고프로"]
 
 function management() {    
     console.log("어서오세요 관리자님")
+    equipmentChoice()
+}
+
+
+function equipmentChoice() {
     console.log("원하시는 서비스를 입력하세요")
-    console.log("1. 장비 조회\n2. 장비 등록\n3. 장비 삭제")
+    console.log("1. 장비 조회\n2. 장비 등록\n3. 장비 삭제\n4. 종료")
     rl.question("", (line : string) => {      
-        let choice = parseInt(line)  
+        let choice = parseInt(line)
         switch (choice) {
             case 1: // 장비 조회
                 printAllEquipemt()
@@ -34,15 +39,18 @@ function management() {
                 console.log("장비 등록")
                 break
             case 3 : // 장비 삭제
-                console.log("장비 삭제")    
-                break                
+                console.log("장비 삭제")
+                break
+            case 4 : // 종료
+                console.log("종료 됩니다")
+                process.exit()
             default : // 잘못된 입력
+                console.log("잘못된 입력입니다.")
                 break
         }
+        equipmentChoice()
     })
-    
 }
-
 
 // 모든 장비를 조회합니다.
 function printAllEquipemt() {
